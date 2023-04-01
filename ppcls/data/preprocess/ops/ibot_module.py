3e012f82a230c8796e14814b90099074d26d26e6
PommesPeter <434596665@qq.com>
2023-04-01 23:12:41 +0800

    updated data augmentation function

--- ppcls/data/preprocess/ops/ibot_module.py
+++ ppcls/data/preprocess/ops/ibot_module.py
@@ -1,8 +1,9 @@
-from paddle.vision import transforms
-# from paddle.vision import BaseTransform
-from PIL import ImageFilter, ImageOps
 import random
-# import paddle
+
+from paddle.vision import transforms
+from paddle.vision.transforms import transforms as T
+from PIL import ImageFilter, ImageOps
+
 from ppcls.data.preprocess import RandomApply, RandomGrayscale
 
 
@@ -26,6 +27,7 @@ class GaussianBlur(object):
             )
         )
 
+
 class Solarization(object):
     """
     Apply Solarization to the PIL image.
@@ -40,51 +42,35 @@ class Solarization(object):
             return img
 
 
-class DataAugmentationiBOT(object):
-    def __init__(self, global_crops_scale, local_crops_scale, global_crops_number, local_crops_number):
-        flip_and_color_jitter = transforms.Compose([
-            transforms.RandomHorizontalFlip(prob=0.5),
-            RandomApply(
-                p=0.8,
-                transforms=[transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
-            ),
-            RandomGrayscale(p=0.2),
-        ])
-        normalize = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        ])
+class IBOTAugmentation(object):
+    def __init__(self, 
+                 transform_ops_global,
+                 transform_ops_local,
+                 global_crops_number, 
+                 local_crops_number):
+        self.global_number = global_crops_number
+        self.local_number = local_crops_number
+        
+        ts_global = []
+        for t in transform_ops_global:
+            for key in t.keys():
+                ts_global.append(eval(key)(**t[key]))
+        ts_global2 = ts_global.copy()
+        _ = ts_global2.pop(-1)
+        self.trans_global = T.Compose(ts_global)
+        self.trans_global2 = T.Compose(ts_global2)
+        
+        ts_local = []
+        for t in transform_ops_local:
+            for key in t.keys():
+                ts_local.append(eval(key)(**t[key]))
+        self.trans_local = T.Compose(ts_local)
 
-        self.global_crops_number = global_crops_number
-        # transformation for the first global crop
-        self.global_transfo1 = transforms.Compose([
-            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation="bicubic"),
-            flip_and_color_jitter,
-            GaussianBlur(1.0),
-            normalize,
-        ])
-        # transformation for the rest of global crops
-        self.global_transfo2 = transforms.Compose([
-            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation="bicubic"),
-            flip_and_color_jitter,
-            GaussianBlur(0.1),
-            Solarization(0.2),
-            normalize,
-        ])
-        # transformation for the local crops
-        self.local_crops_number = local_crops_number
-        self.local_transfo = transforms.Compose([
-            transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation="bicubic"),
-            flip_and_color_jitter,
-            GaussianBlur(p=0.5),
-            normalize,
-        ])
-
-    def __call__(self, image):
-        crops = []
-        crops.append(self.global_transfo1(image))
-        for _ in range(self.global_crops_number - 1):
-            crops.append(self.global_transfo2(image))
-        for _ in range(self.local_crops_number):
-            crops.append(self.local_transfo(image))
-        return crops
+    def __call__(self, img):
+        global_crops_imgs = [self.trans_global(img) 
+                             for o in range(self.global_number - 1)]
+        global_crops_imgs.insert(0, self.trans_global2(img))
+        local_crops_imgs = [self.trans_local(img) 
+                             for o in range(self.local_number)]
+        
+        return global_crops_imgs + local_crops_imgs
